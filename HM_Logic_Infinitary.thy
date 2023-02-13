fdce97941a8f91e1f7c2377de4074aefeea17c1f
benkeks <benjamin.bisping@tu-berlin.de>
2023-02-13 22:01:57 +0100

    Begin Hennessy-Milner proof for Contrasim (...hopefully...)

--- HM_Logic_Infinitary.thy
+++ HM_Logic_Infinitary.thy
@@ -7,9 +7,9 @@ chapter \<open>Infinitary Hennessy-Milner Logic\<close>
 
 datatype ('a,'x)HML_formula =
   HML_true 
-| HML_conj \<open>'x set\<close> \<open>'x \<Rightarrow> ('a,'x)HML_formula\<close> 
-| HML_neg \<open>('a,'x)HML_formula\<close> 
-| HML_poss \<open>'a\<close> \<open>('a,'x)HML_formula\<close> 
+| HML_conj \<open>'x set\<close> \<open>'x \<Rightarrow> ('a,'x)HML_formula\<close>  (\<open>AND _ _\<close>)
+| HML_neg \<open>('a,'x)HML_formula\<close>                  (\<open>~_\<close>)
+| HML_poss \<open>'a\<close> \<open>('a,'x)HML_formula\<close>            (\<open>\<langle>_\<rangle>_\<close>)
 
 subsection \<open>Satisfaction Relation\<close>
 
@@ -33,7 +33,7 @@ inductive_set HML_wf_rel :: \<open>('s \<times> ('a, 's) HML_formula) rel\<close
 
 lemma HML_wf_rel_is_wf: \<open>wf HML_wf_rel\<close> 
   unfolding wf_def
-proof (rule allI, rule impI, rule allI)
+proof (safe)
   fix P::\<open>'s \<times> ('a, 's) HML_formula \<Rightarrow> bool\<close> and t::\<open>'s \<times> ('a, 's) HML_formula\<close>
   obtain p \<phi> where \<open>t = (p, \<phi>)\<close> by force
   assume \<open>\<forall>x. (\<forall>y. (y, x) \<in> HML_wf_rel \<longrightarrow> P y) \<longrightarrow> P x\<close>
@@ -49,6 +49,33 @@ qed
 
 termination satisfies using HML_wf_rel_is_wf 
   by (standard, (simp add: HML_wf_rel.intros)+)
+
+
+inductive_set HML_direct_subformulas :: \<open>(('a, 's) HML_formula) rel\<close>
+  where
+    \<open>\<phi> = F i \<and> i \<in> I \<Longrightarrow> (\<phi>, HML_conj I F) \<in> HML_direct_subformulas\<close> 
+  | \<open>(\<phi>, HML_neg \<phi>) \<in> HML_direct_subformulas\<close> 
+  | \<open>(\<phi>, HML_poss \<alpha> \<phi>) \<in> HML_direct_subformulas\<close>
+
+
+lemma HML_direct_subformulas_wf: \<open>wf HML_direct_subformulas\<close> 
+  unfolding wf_def
+proof (safe)
+  fix P x
+  assume \<open>\<forall>x. (\<forall>y. (y, x) \<in> HML_direct_subformulas \<longrightarrow> P y) \<longrightarrow> P x\<close>
+  thus \<open>P x\<close>
+    apply (induct, auto)
+    using HML_direct_subformulas.simps apply blast
+    apply (metis HML_direct_subformulas.cases HML_formula.distinct(7) HML_formula.distinct(9) HML_formula.inject(1) range_eqI)
+    apply (metis HML_direct_subformulas.simps HML_formula.distinct(11) HML_formula.distinct(7) HML_formula.inject(2))
+    by (metis HML_direct_subformulas.simps HML_formula.distinct(11) HML_formula.distinct(9) HML_formula.inject(3))
+qed
+
+definition HML_subformulas where \<open>HML_subformulas \<equiv> (HML_direct_subformulas)\<^sup>+\<close>
+
+lemma HML_subformulas_wf: \<open>wf HML_subformulas\<close>
+  using HML_direct_subformulas_wf HML_subformulas_def wf_trancl
+  by fastforce
 
 subsection \<open>Distinguishing Formulas\<close>
 
@@ -66,12 +93,6 @@ lemma HML_equivalent_symm:
   assumes \<open>HML_equivalent p q\<close>
   shows \<open>HML_equivalent q p\<close>
   using HML_equivalent_def assms by presburger
-
-subsection \<open>Weak-NOR HML\<close>
-
-definition HML_weaknor ::
-  \<open>'x set \<Rightarrow> ('x \<Rightarrow> ('a,'x)HML_formula) \<Rightarrow> ('a,'x)HML_formula\<close>
-  where \<open>HML_weaknor I F = HML_poss \<tau> (HML_conj I (\<lambda>f. HML_neg (F f)))\<close>
 
 end \<comment> \<open>of \<open>context lts\<close>\<close>
 end \<comment> \<open>of \<open>theory\<close>\<close>
